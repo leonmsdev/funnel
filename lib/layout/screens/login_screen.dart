634cb9fc79f418dbbde64492d709f6686221eb53
leonmsdev <leonms.dev@gmail.com>
2022-08-19 17:52:24 +0200

    fix(): fixing macos cocopod version error

---
+++ lib/layout/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Center(
+            child: Column(
+      children: const [Text("Hello")],
+    )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:funnel/layout/layout.dart';
 import 'package:funnel/providers/color_theme_provider.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
+import 'layout/screens/login_screen.dart';
+
 bool isDarkMode = false;
 
-void main() {
-  runApp(
-    MyApp(),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => ThemeProvider()..initialize(),
-      child: Master(),
+      child: LoginScreen(),
     );
   }
 }
