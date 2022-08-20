a0538b3faa0ab000cd2c27b970d8cc83f9fde844
leonmsdev <leonms.dev@gmail.com>
2022-08-20 12:57:22 +0200

    feature(ui): add login ui page

--- lib/layout/screens/login_screen.dart
+++ lib/layout/screens/login_screen.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Expanded(
-        child: Center(
-            child: Column(
-      children: const [Text("Hello")],
-    )));
+    return Center(
+      child: SizedBox(
+        width: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            const SizedBox(height: 50),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.login),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
